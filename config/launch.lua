d2bfe383c5c5c4496894571a67561b20e1f949ab
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2025-10-16 14:04:57 +0800

    Config Sokyoei's launch and format code

--- config/launch.lua
+++ config/launch.lua
@@ -8,30 +8,46 @@ local options = {
 if platform.is_win then
    options.default_prog = { 'pwsh', '-NoLogo' }
    options.launch_menu = {
-      { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
+      { label = 'Windows PowerShell', args = { 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe' } },
+      { label = 'PowerShell Core',    args = { 'pwsh', '-NoLogo' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
-      { label = 'Command Prompt', args = { 'cmd' } },
-      { label = 'Nushell', args = { 'nu' } },
-      { label = 'Msys2', args = { 'ucrt64.cmd' } },
+      { label = 'Command Prompt',     args = { 'cmd' } },
+      -- { label = 'Nushell',            args = { 'nu' } },
       {
-         label = 'Git Bash',
-         args = { 'C:\\Users\\kevin\\scoop\\apps\\git\\current\\bin\\bash.exe' },
+         label = 'Anaconda Powershell Prompt',
+         args = {
+            'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe', '-ExecutionPolicy', 'ByPass', '-NoExit',
+            '-Command', "& 'D:\\Anaconda3\\shell\\condabin\\conda-hook.ps1' ; conda activate 'D:\\Anaconda3' "
+         }
+      },
+      {
+         label = 'Anaconda Command Prompt',
+         args = {
+            'c:\\Windows\\System32\\cmd.exe', "/K", 'D:\\Anaconda3\\Scripts\\activate.bat', 'D:\\Anaconda3'
+         }
+      },
+      { label = 'UCRT64 / MSYS2', args = { 'D:/msys64/msys2_shell.cmd', '-defterm', '-here', '-no-start', '-ucrt64' } },
+      {
+         label = 'Git Bash(D:/Program Files/Git)', args = { 'D:/Program Files/Git/bin/bash.exe', '-i', '-l' },
+      },
+      {
+         label = 'Git Bash(D:/Git)', args = { 'D:/Git/bin/bash.exe', '-i', '-l' },
       },
    }
 elseif platform.is_mac then
    options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
    options.launch_menu = {
-      { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { '/opt/homebrew/bin/fish', '-l' } },
+      { label = 'Bash',    args = { 'bash', '-l' } },
+      { label = 'Fish',    args = { '/opt/homebrew/bin/fish', '-l' } },
       { label = 'Nushell', args = { '/opt/homebrew/bin/nu', '-l' } },
-      { label = 'Zsh', args = { 'zsh', '-l' } },
+      { label = 'Zsh',     args = { 'zsh', '-l' } },
    }
 elseif platform.is_linux then
-   options.default_prog = { 'fish', '-l' }
+   options.default_prog = { 'bash', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
       { label = 'Fish', args = { 'fish', '-l' } },
-      { label = 'Zsh', args = { 'zsh', '-l' } },
+      { label = 'Zsh',  args = { 'zsh', '-l' } },
    }
 end
 
--- wezterm.lua
+++ wezterm.lua
@@ -7,9 +7,9 @@ if data_dir and data_dir ~= '' then
 end
 
 require('utils.backdrops')
-   -- :set_focus('#000000')
-   :set_images()
-   :random()
+--  :set_focus('#000000')
+    :set_images()
+    :random()
 
 require('events.left-status').setup()
 require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
@@ -18,9 +18,9 @@ require('events.new-tab-button').setup()
 require('events.gui-startup').setup()
 
 return Config:init()
-   :append(require('config.appearance'))
-   :append(require('config.bindings'))
-   :append(require('config.domains'))
-   :append(require('config.fonts'))
-   :append(require('config.general'))
-   :append(require('config.launch')).options
+    :append(require('config.appearance'))
+    :append(require('config.bindings'))
+    :append(require('config.domains'))
+    :append(require('config.fonts'))
+    :append(require('config.general'))
+    :append(require('config.launch')).options
